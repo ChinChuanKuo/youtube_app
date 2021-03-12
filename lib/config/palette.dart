99c49915306169b5ecd6d75b9ea8e4371cdc3e98
ChinChuanKuo <chinchuankuo@ChinChuanKuos-MacBook-Pro.local>
2021-03-12 18:06:43 +0800

    module better

--- lib/config/palette.dart
+++ lib/config/palette.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
 class Palette {
-  static const Color facebookBlue = Color(0xFF1777F2);
+  static const Color facebookBlue = Colors.black54;
 }
--- lib/data/screen.dart
+++ lib/data/screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 final List<List> moreOption = const [
   [Icons.home, Colors.red, '首頁', false, true],
-  [Icons.hot_tub, Colors.black54, '發燒影片', false, true],
+  [Icons.whatshot, Colors.black54, '發燒影片', false, true],
   [Icons.subscriptions, Colors.black54, '訂閱內容', false, false],
   [null, null, null, true, null]
 ];
